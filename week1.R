b38c41dc8f05140cecee74dcf0c961c1c9d00c97
Edde Yang <edde.yang@activenetwork.com>
2016-10-31 23:12:46 +0800

    add old test script

--- week1.R
+++ week1.R
@@ -1,3 +1,51 @@
+install.packages("devtools")
+library(devtools)
+install_github("genomicsclass/dagdata")
+install_github("ririzarr/rafalib")
+
+install.packages("swirl")
+library(swirl)
+swirl()
+
+# In swirl, we can do...
+#=======================
+# | When you are at the R prompt (>):
+#     | -- Typing skip() allows you to skip the current question.
+# | -- Typing play() lets you experiment with R on your own; swirl will ignore what you do...
+# | -- UNTIL you type nxt() which will regain swirl's attention.
+# | -- Typing bye() causes swirl to exit. Your progress will be saved.
+# | -- Typing main() returns you to swirl's main menu.
+# | -- Typing info() displays these options again.
+
+v <- c(2.23, 3.45, 1.87, 2.11, 7.33, 18.34, 19.23)
+
+
+# Use a for loop to determine the value of ∑i=125i2
+# Remember to execute these three line in a batch
+total <- 0
+for(i in 1:25) {total <- total + i^2}
+total
+
+
+# Use nrow to count how many rows in a data.frame
+nrow(cars)
+# Use ncol to count how many columns in a data.frame
+ncol(cars)
+
+
+# What is the name of the second column of cars?
+names(cars)[2]
+
+
+# The simplest way to extract the columns of a matrix or data.frame is
+# using [. For example you can access the second column with cars[,2].
+# What is the average distance traveled in this dataset?
+mean(cars[,2])
+
+
+# Familiarize yourself with the which function. What row of cars has a a distance of 85?
+which(cars$dist == 85)
+
 setwd("c:/my_projects/PH525.1x Statistics and R/")
 data <- read.csv("./dagdata/inst/extdata/femaleMiceWeights.csv")
 
